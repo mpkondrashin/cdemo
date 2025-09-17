b20716204515b696bb53954dbd903ae57b65784e
Michael Kondrashin <mkondrashin@gmail.com>
2025-09-17 10:23:47 +0300

    latest go

---
+++ file_security/fscan/get_latest_go.sh
@@ -0,0 +1,7 @@
+curl -fsSL https://go.dev/dl/ | grep -oP 'go[0-9]+\.[0-9]+\.[0-9]+\.linux-amd64\.tar\.gz' | head -n1 | xargs -I {} sh -c '
+  cd /tmp &&
+  curl -LO https://go.dev/dl/{} &&
+  sudo rm -rf /usr/local/go &&
+  sudo tar -C /usr/local -xzf {} &&
+  echo "export PATH=\$PATH:/usr/local/go/bin" | sudo tee /etc/profile.d/go.sh > /dev/null
+'
